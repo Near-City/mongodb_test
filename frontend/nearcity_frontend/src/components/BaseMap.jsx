04a3f1f1f6f4f992193454ee2505c15cb31cef8e
Near-City <nearcity@upv.es>
2025-02-06 12:02:29 +0100

    06022025v2

--- frontend/nearcity_frontend/src/components/BaseMap.jsx
+++ frontend/nearcity_frontend/src/components/BaseMap.jsx
@@ -172,7 +172,7 @@ const BaseMap = ({
       onClick: () => handleExtraClick("red"),
       id: "red",
     },
-    },
+    
   ];
 
   return (
@@ -201,13 +201,7 @@ const BaseMap = ({
           onPolygonClick={onIsocronaClick}
         />
         <FilterManager config={config} geojsonData={areasData?.polygons} />
-        <FilterManager config={config} geojsonData={areasData?.polygons} />
         <LocsManager config={config} geojsonData={areasData?.locs} />
-        <ExtraManager
-          config={config}
-          geojsonData={areasData?.extra}
-          activeExtra={activeExtraButtons}
-        />
         <ExtraManager
           config={config}
           geojsonData={areasData?.extra}
@@ -253,54 +247,12 @@ const BaseMap = ({
             />
           )}
 
-          {/* <SwipeMenu
-          {swipeMenuOpen ? (
-            <>
-              <IndicatorSelector
-                position="left-center"
-                layout="column"
-                indicatorName="primary"
-                startAngle={-40}
-                rotationAngle={90}
-                radius={9}
-                selectedTimeElement={selectedTimeElementPrimary}
-                setSelectedTimeElement={setSelectedTimeElementPrimary}
-                gap={32}
-              />
-              <IndicatorSelector
-                position="right-center"
-                layout="column"
-                indicatorName="secondary"
-                startAngle={140}
-                rotationAngle={90}
-                radius={9}
-                selectedTimeElement={selectedTimeElementSecondary}
-                setSelectedTimeElement={setSelectedTimeElementSecondary}
-                gap={8}
-              />
-            </>
-          ) : (
-            <IndicatorSelector position="bottom-center" layout="row" 
-              selectedTimeElement={selectedTimeElementPrimary}
-              setSelectedTimeElement={setSelectedTimeElementPrimary}
-            />
-          )}
-
-          {/* <SwipeMenu
-            isMenuOpen={swipeMenuOpen}
-            onMenuToggle={handleSwipeMenuToggle}
-          /> */}
-          /> */}
+          
           <TileSelector
             isSatellite={isSatellite}
             onClick={toggleLayer}
             activeExtraButtons={activeExtraButtons}
             setActiveExtraButtons={setActiveExtraButtons}
-          />
-          <DebouncedSearchBar
-            onSearch={handleSearch}
-            results={searchResults}
-            onResultClick={onResultClick}
           />
           <DebouncedSearchBar
             onSearch={handleSearch}
